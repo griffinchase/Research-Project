90ec21ef53df1f36effcbe3e8f1a169eba9f00e5
zcqsgki <zcqsgki@ucl.ac.uk>
2023-11-29 15:24:48 +0000

    updates

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='simpleABC',
+    version='0.1',
+    packages=['simpleABC'],
+    url='',
+    license='',
+    author='AJFed',
+    author_email='alexander.fedorec.13@ucl.ac.uk',
+    description='simple package for running ABC on SBML models'
+)
